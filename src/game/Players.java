12f9b405b691ace81c5339ac70107df5ab15f048
JCT03 <119342826+JCT03@users.noreply.github.com>
2022-11-28 10:43:24 -0600

    Created Players ENUM

--- src/game/Players.java
+++ src/game/Players.java
@@ -1,4 +1,34 @@
 package game;
 
+import javafx.scene.paint.Color;
+
 public enum Players {
+    EMPTY {
+        @Override
+        public Color getFill(){return Color.BURLYWOOD;}
+        @Override
+        public String getText() {return "";}
+        @Override
+        public Players getOpponent(){return Players.EMPTY;}
+    },
+    RED {
+        @Override
+        public Color getFill(){return Color.RED;}
+        @Override
+        public String getText() {return "Red Player";}
+        @Override
+        public Players getOpponent(){return Players.BLACK;}
+    },
+    BLACK {
+        @Override
+        public Color getFill(){return Color.BLACK;}
+        @Override
+        public String getText() {return "Black Player";}
+        @Override
+        public Players getOpponent(){return Players.RED;}
+    };
+    abstract public Color getFill();
+    abstract public String getText();
+    abstract public Players getOpponent();
+
 }
