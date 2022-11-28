994817a12f30e5694585854fb0358cee1f132f71
JCT03 <119342826+JCT03@users.noreply.github.com>
2022-11-28 10:51:47 -0600

    Created Quadrants and Rotation enums

--- src/game/Quadrants.java
+++ src/game/Quadrants.java
@@ -1,4 +1,21 @@
 package game;
 
 public enum Quadrants {
+    Q1 {
+        @Override
+        public String getText() {return "top right";}
+    },
+    Q2 {
+        @Override
+        public String getText() {return "top left";}
+    },
+    Q3 {
+        @Override
+        public String getText() {return "bottom left";}
+    },
+    Q4 {
+        @Override
+        public String getText() {return "bottom right";}
+    };
+    abstract public String getText();
 }
--- src/game/Rotation.java
+++ src/game/Rotation.java
@@ -1,4 +1,18 @@
 package game;
 
 public enum Rotation {
+    CLOCKWISE {
+        @Override
+        public Rotation getOpposite() {return Rotation.COUNTERCLOCKWISE;}
+        @Override
+        public String getText() {return "clockwise";}
+    },
+    COUNTERCLOCKWISE {
+        @Override
+        public Rotation getOpposite() {return Rotation.CLOCKWISE;}
+        @Override
+        public String getText() {return "counterclockwise";}
+    };
+    abstract public Rotation getOpposite();
+    abstract public String getText();
 }
